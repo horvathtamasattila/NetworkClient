63fbd5abdcbf19d59aa5ab7b7117ec8709149f0b
Horvath Tamas <tamas.horvath@makery.co>
2021-03-21 12:01:00 +0100

    add: dependencies

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "NetworkClient",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -12,17 +13,28 @@ let package = Package(
             targets: ["NetworkClient"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "NetworkClient",
-            dependencies: []),
-        .testTarget(
-            name: "NetworkClientTests",
-            dependencies: ["NetworkClient"]),
+            dependencies: [
+            ]),
+        .binaryTarget(
+            name: "Alamofire",
+            url: "https://github.com/horvathtamasattila/Dependencies/releases/download/1.0.0/Alamofire.xcframework.zip",
+            checksum: "2b434ef055433edc7b462562e6571d762704d39451c9add30e234f101361eb7b"
+        ),
+        .binaryTarget(
+            name: "FBSDKCoreKit",
+            url: "https://github.com/horvathtamasattila/Dependencies/releases/download/1.0.0/FBSDKCoreKit.xcframework.zip",
+            checksum: "d9f7410c48f5dabdae679d533dec30f2aac7e542c407421cd29822aff5ea3e3c"
+        ),
+        .binaryTarget(
+            name: "FBSDKLoginKit",
+            url: "https://github.com/horvathtamasattila/Dependencies/releases/download/1.0.0/FBSDKLoginKit.xcframework.zip",
+            checksum: "e9a1191feaaa8290a755701c68da82059b93068968cffd77a8c58e76573c0b5e"
+        )
+
     ]
 )
