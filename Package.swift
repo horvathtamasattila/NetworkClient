5822f5c44b5fa1aa92e4cec891b040316c058f77
Horvath Tamas <tamas.horvath@makery.co>
2021-03-22 10:18:50 +0100

    update: remove comments

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -7,7 +6,6 @@ let package = Package(
     name: "NetworkClient",
     platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "NetworkClient",
             targets: ["NetworkClient"]
